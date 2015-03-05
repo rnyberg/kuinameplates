6985133efdc34927b0fd2092de6ae25c0fddf8f4
kesava-wow <kesava-wow@users.noreply.github.com>
2015-03-05 19:40:50 +0000

    add option to disable classcolours.lua

--- Kui_Nameplates/classcolours.lua
+++ Kui_Nameplates/classcolours.lua
@@ -33,14 +33,12 @@ function mod:GUIDStored(msg, f, unit)
         if #cache_index > 100 then
             cache[tremove(cache_index, 1)] = nil
         end
-
-        print(#cache_index)
     end
 end
 
 function mod:PostShow(msg, f)
-    -- restore colour from cache
     if cache[f.name.text] then
+        -- restore colour from cache
         self:SetClassColour(f, cc_table[cache[f.name.text]])
     elseif f.friend and f.player then
         -- a friendly player with no class information
@@ -54,13 +52,47 @@ function mod:PostHide(msg, f)
     f.name:SetTextColor(1,1,1,1)
 end
 
+mod.configChangedFuncs = { runOnce = {} }
+mod.configChangedFuncs.runOnce.friendly = function(v)
+    if v then
+        mod:Enable()
+    else
+        mod:Disable()
+    end
+end
+
+function mod:GetOptions()
+    return {
+        friendly = {
+            name = 'Class colour friendly player names',
+            desc = 'Class colour the names of friendly players and dim the names of friendly players with no class information. Note that friendly players will only become class coloured once you mouse over their frames, at which point their class will be cached.',
+            type = 'toggle',
+            order = 10
+        }
+    }
+end
+
 function mod:OnInitialize()
     cc_table = CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS
-    self:SetEnabledState(true)
+
+    self.db = addon.db:RegisterNamespace(self.moduleName, {
+        profile = {
+            friendly = true
+        }
+    })
+
+    addon:InitModuleOptions(self)
+    self:SetEnabledState(self.db.profile.friendly)
 end
 
 function mod:OnEnable()
     self:RegisterMessage('KuiNameplates_GUIDStored', 'GUIDStored')
     self:RegisterMessage('KuiNameplates_PostShow', 'PostShow')
     self:RegisterMessage('KuiNameplates_PostHide', 'PostHide')
+end
+
+function mod:OnDisable()
+    self:UnregisterMessage('KuiNameplates_GUIDStored', 'GUIDStored')
+    self:UnregisterMessage('KuiNameplates_PostShow', 'PostShow')
+    self:UnregisterMessage('KuiNameplates_PostHide', 'PostHide')
 end
