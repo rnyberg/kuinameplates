9da4d19a53b8cdd2697526627c4e4577bf356983
kesava-wow <kesava-wow@users.noreply.github.com>
2015-09-14 22:58:11 +0100

    disable on target, colour npc name

--- Kui_Nameplates/modules/nameonly.lua
+++ Kui_Nameplates/modules/nameonly.lua
@@ -12,11 +12,15 @@ local function SwitchOn(f)
     f.nameonly = true
 
     f:CreateFontString(f.name, {
-        reset = true, size = f.trivial and 'small' or 'large', shadow = true
+        reset = true, size = f.trivial and 'small' or 'name', shadow = true
     })
     f.name:SetParent(f)
     f.name:ClearAllPoints()
     f.name:SetPoint('CENTER')
+
+    if f.friend and not f.player then
+        f.name:SetTextColor(.6,1,.6)
+    end
 
     f.health:Hide()
     f.overlay:Hide()
@@ -33,16 +37,27 @@ local function SwitchOff(f)
     f.name:SetParent(f.overlay)
     f.name:ClearAllPoints()
 
+    if f.friend and not f.player then
+        f.name:SetTextColor(1,1,1)
+    end
+
     f.health:Show()
     f.overlay:Show()
     f.bg:Show()
 
+    -- reposition name
     addon:UpdateName(f,f.trivial)
 end
 
 local function OnHealthValueChanged(oldHealth)
     local f = oldHealth.kuiParent.kui
-    if f.nameonly_ignore then return end
+
+    if not f.friend then
+        if f.nameonly then
+            SwitchOff(f)
+        end
+        return
+    end
 
     local _,max = oldHealth:GetMinMaxValues()
     local cur = oldHealth:GetValue()
@@ -55,18 +70,21 @@ local function OnHealthValueChanged(oldHealth)
 end
 
 function mod:PostShow(msg,f)
-    if not f.friend then
-        f.nameonly_ignore = true
-    else
-        f.nameonly_ignore = nil
-        OnHealthValueChanged(f.oldHealth)
-    end
+    OnHealthValueChanged(f.oldHealth)
 end
 function mod:PostHide(msg,f)
     SwitchOff(f)
 end
 function mod:PostCreate(msg,f)
     f.oldHealth:HookScript('OnValueChanged',OnHealthValueChanged)
+end
+function mod:PostTarget(msg,f,is_target)
+    if is_target then
+        -- never nameonly the target
+        SwitchOff(f)
+    else
+        OnHealthValueChanged(f.oldHealth)
+    end
 end
 
 function mod:OnInitialize()
@@ -76,4 +94,5 @@ function mod:OnEnable()
     self:RegisterMessage('KuiNameplates_PostHide','PostHide')
     self:RegisterMessage('KuiNameplates_PostShow','PostShow')
     self:RegisterMessage('KuiNameplates_PostCreate','PostCreate')
+    self:RegisterMessage('KuiNameplates_PostTarget','PostTarget')
 end
